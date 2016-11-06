7288a003df858c0ba9b6497bc75e4e3d1bc74189
John DeSilvio <john.desilvio@yahoo.com>
2016-11-05 20:54:11 -0400

    Moved helpers from DailyFantasy.ex to appropriate location

--- lib/daily_fantasy/players/player.ex
+++ lib/daily_fantasy/players/player.ex
@@ -20,8 +20,8 @@ defmodule DailyFantasy.Players.Player do
     struct(DailyFantasy.Players.Player,
      [name: player["First Name"] <> " " <> player["Last Name"],
       position: player["Position"],
-      points: DailyFantasy.number_string_to_float(player["FPPG"]),
-      salary: DailyFantasy.number_string_to_float(player["Salary"]),
+      points: number_string_to_float(player["FPPG"]),
+      salary: number_string_to_float(player["Salary"]),
       team: player["Team"],
       opponent: player["Opponent"],
       injury_status: player["Injury Indicator"],
@@ -38,6 +38,48 @@ defmodule DailyFantasy.Players.Player do
             player.opponent <> " | Salary: $" <> 
             Integer.to_string(round(player.salary)) <> " | Points: " <> 
             Integer.to_string(round(player.points))
+  end
+
+  @doc ~S"""
+  If a string contains a valid number and only a valid number,
+  then return that number as a float. Otherwise, return the
+  original string. If original value is not a string, return nil.
+
+      iex> DailyFantasy.Players.Player.number_string_to_float "13"
+      13.0
+
+      iex> DailyFantasy.Players.Player.number_string_to_float "13.99"
+      13.99
+
+      iex> DailyFantasy.Players.Player.number_string_to_float "13a"
+      "13a"
+
+      iex> DailyFantasy.Players.Player.number_string_to_float "thirteen"
+      "thirteen"
+
+      iex> DailyFantasy.Players.Player.number_string_to_float "-13"
+      -13.0
+
+      iex> DailyFantasy.Players.Player.number_string_to_float "+13"
+      13.0
+
+      iex> DailyFantasy.Players.Player.number_string_to_float nil
+      nil
+
+      iex> DailyFantasy.Players.Player.number_string_to_float :an_atom
+      nil
+
+  """
+  def number_string_to_float(str) do
+    if is_binary(str) do
+      case Float.parse(str) do
+        {num, ""}  -> num
+        {_num, _r} -> str
+        :error     -> str
+      end
+    else
+      nil
+    end
   end
 
 end
