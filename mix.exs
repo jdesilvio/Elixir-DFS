9d12a58baaeb2b927f4baec24d9d82e3b8f90f4a
John DeSilvio <john.desilvio@yahoo.com>
2016-12-19 22:10:07 -0500

    Using excoveralls for test coverage

--- mix.exs
+++ mix.exs
@@ -7,33 +7,26 @@ defmodule DailyFantasy.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: ["coveralls": :test,
+                         "coveralls.detail": :test,
+                         "coveralls.post": :test,
+                         "coveralls.html": :test]]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
      mod: {DailyFantasy, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:csv, "~> 1.4.2"},
       {:credo, "~> 0.4", only: [:dev, :test]},
       {:combination, "~> 0.0.2"},
-      {:benchwarmer, "~> 0.0.2"},
-      {:benchfella, "~> 0.3.0"}
+      {:benchfella, "~> 0.3.0"},
+      {:excoveralls, "~> 0.5", only: :test}
     ]
   end
 end
