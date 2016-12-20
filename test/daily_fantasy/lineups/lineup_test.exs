6df1f7d38414e5c1071ff14a58450b5c31aefeba
John DeSilvio <john.desilvio@yahoo.com>
2016-12-19 20:07:33 -0500

    Added Lineup module tests

---
+++ test/daily_fantasy/lineups/lineup_test.exs
@@ -0,0 +1,26 @@
+defmodule DailyFantasyTests.LineupsTests.LineupTests do
+  use ExUnit.Case
+
+  alias DailyFantasy.Lineups.Lineup
+
+  @ player_essential_tuples [{0, :C, 0, 0},
+                             {1, :C, 0, 0},
+                             {2, :C, 0, 0},
+                             {3, :PG, 0, 0},
+                             {4, :PG, 0, 0},
+                             {5, :PG, 0, 0}]
+
+  test "position mapping - no combinations" do
+    position_map =  Lineup.map_position(@player_essential_tuples, :C, 1)
+    assert length(position_map) == 3
+    assert length(List.flatten(position_map)) == 3
+
+  end
+
+  test "position mapping - with combinations" do
+    position_map = Lineup.map_position(@player_essential_tuples, :PG, 2)
+    assert length(position_map) == 3
+    assert length(List.flatten(position_map)) == 6
+  end
+
+end
