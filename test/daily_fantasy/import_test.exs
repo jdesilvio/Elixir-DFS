c4aff9001b6b6515866fed00ce414a3b30b25091
John DeSilvio <john.desilvio@yahoo.com>
2016-12-28 14:50:39 -0500

    Fixed timing bug in Import test

--- test/daily_fantasy/import_test.exs
+++ test/daily_fantasy/import_test.exs
@@ -4,28 +4,22 @@ defmodule DailyFantasyTests.ImportTests do
   alias DailyFantasy.Import
   alias DailyFantasy.Players.Player
 
-  @player %Player{name: "Michael Jordan",
-                  team: :CHI,
-                  position: :SG,
-                  points: 55.9,
-                  salary: 8900,
-                  opponent: :WORLD,
-                  injury_details: "",
-                  injury_status: :""}
-
   setup_all do
     Import.register(:nba_fixture)
+    :timer.sleep(1000) # Wait for table to be setup
+  end
+
+  test "registry contains players" do
+    [h|_t] = :ets.lookup(:player_registry, 0)
+
+    assert elem(h, 0) == 0
   end
 
   test "player registry exists" do
     assert :player_registry in :ets.all == true
   end
 
-  test "registry contains players" do
-    assert :ets.lookup(:player_registry, 0) == [{0, @player}]
-  end
-
-  test "Invalid registry" do
+  test "invalid registry" do
     catch_exit Import.register(:does_not_exist)
   end
 
